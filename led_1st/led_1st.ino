9437b1ae2ef65dcbf795e77771cad334e4914e31
Takeshi Ueda <uedatakeshi@gmail.com>
2015-02-15 11:01:16 +0900

    Add first led test

---
+++ led_1st/led_1st.ino
@@ -0,0 +1,14 @@
+#define LED_PIN 10
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
